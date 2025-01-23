59e1da474d244c1f446f11673bc43802ba48aa82
𝐿𝑒𝑎𝑓𝐸𝑣𝑎𝑛𝑠 <140964616+LeafEvans@users.noreply.github.com>
2025-01-24 02:11:22 +0800

    Delete Practice/Rust/test_2025_1_22/target directory

--- Practice/Rust/test_2025_1_22/target/debug/deps/test_2025_1_22-c688d57d2a70391e.d
+++
@@ -1,5 +0,0 @@
-/home/leafevans/Code/Code_Practice/Practice/Rust/test_2025_1_22/target/debug/deps/libtest_2025_1_22-c688d57d2a70391e.rmeta: src/main.rs
-
-/home/leafevans/Code/Code_Practice/Practice/Rust/test_2025_1_22/target/debug/deps/test_2025_1_22-c688d57d2a70391e.d: src/main.rs
-
-src/main.rs:
