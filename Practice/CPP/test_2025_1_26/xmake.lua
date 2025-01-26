cbb17b57029eac08ed251014e5127f6898e81fd3
LeafEvans <leafevans@foxmail.com>
2025-01-27 01:45:27 +0800

    Update xmake.lua

--- Practice/CPP/test_2025_1_26/xmake.lua
+++ Practice/CPP/test_2025_1_26/xmake.lua
@@ -2,7 +2,7 @@ add_rules("mode.debug", "mode.release")
 
 target("test_2025_1_26")
     set_kind("binary")
-    add_files("src/*.cpp")
+    add_files("src/*.cc")
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
