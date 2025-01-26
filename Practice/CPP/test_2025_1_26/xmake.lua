9a9b2e11e6be7d54485e771a03cf2b927b4fa121
LeafEvans <leafevans@foxmail.com>
2025-01-26 21:29:09 +0800

    Delete Old files

--- Practice/CPP/test_2025_1_26/xmake.lua
+++ Practice/CPP/test_2025_1_26/xmake.lua
@@ -1,8 +1,8 @@
 add_rules("mode.debug", "mode.release")
 
-target("test_2025_1_21")
+target("test_2025_1_26")
     set_kind("binary")
-    add_files("src/*.cc")
+    add_files("src/*.cpp")
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
