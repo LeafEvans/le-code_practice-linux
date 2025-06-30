a3a6e3a0dc06081876a643b3883b68b6fa40fe4f
LeafEvans <leafevans@foxmail.com>
2025-06-30 11:40:12 +0800

    Update files

--- practice/c/test_2025_6_30/src/main.c
+++ practice/c/test_2025_6_30/src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("hello world!\n");
+    for (int i = 0; i < 10; ++i) {
+        printf("%d ", i);
+    }
+    printf("Hello World!");
+    putchar('\n');
     return 0;
 }
