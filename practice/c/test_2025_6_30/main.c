607eb68663fd30d07a028abb7361e93dcbc92819
LeafEvans <leafevans@foxmail.com>
2025-08-29 04:35:31 +0800

    Update files

--- practice/c/test_2025_6_30/main.c
+++ practice/c/test_2025_6_30/main.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define N 10000
+
+typedef struct {
+    int number;     // 学号
+    char name[30];  // 姓名
+    char major[30]; // 专业
+} StudentInfo;
 
 int main(void) {
-    FILE *fp = fopen("../test.txt", "w");
+    FILE* fp = fopen("../students.info", "w"); // 以“写”方式打开文件
+
     if (fp == NULL) {
-        printf("打开文件失败！\n");
-        return 1; 
+        printf("打开文件失败！");
+        exit(0);
     }
-    
-    for (int i = 0; i < 10000; ++i) {
-        if (fputs("Hello World!\n", fp) == EOF) { 
-            printf("写入文件失败！\n");
-            fclose(fp);
-            return 1;
-        }
+
+    StudentInfo student[N];       // 用数组保存 N 个学生的信息
+    for (int i = 0; i < N; ++i) { // 生存 N 个学生的信息
+        student[i].name[0] = '?';
+        student[i].number = i;
+        student[i].major[0] = '?';
     }
+
+    // 将 N 个学生的信息写入文件
+    fwrite(student, sizeof(StudentInfo), N, fp);
     fclose(fp);
-    printf("文件写入成功！\n");
+
     return 0;
 }
